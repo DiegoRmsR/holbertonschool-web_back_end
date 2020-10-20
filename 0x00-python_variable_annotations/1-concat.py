9de68b92ac7f27e53d43ca5e6fcedb2a10522e76
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-10-20 04:20:43 +0000

    [Feature] Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,8 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 
 """ Basic annotations concat """
 
 
 def concat(str1: str, str2: str) -> str:
-	""" Return two strings concatenated """
+    """ Return two strings concatenated """
     return str1 + str2
