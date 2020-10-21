1a0957cc60645462eaeba620ddc3ebe50d0084b2
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-10-21 04:56:03 +0000

    [Feature ] pep8

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').wait_random
 
 
-async def task_wait_n(n: int = 0 , max_delay: int = 10) ->List[float]:
+async def task_wait_n(n: int = 0, max_delay: int = 10) ->List[float]:
     """
     Args:
         max_delay: max wait
