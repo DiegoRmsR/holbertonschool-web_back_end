2912c698c73df8bbb275afd42a463cf927078921
DiegoRmsR <diegoramos.mt@gmail.com>
2020-10-26 18:08:38 -0500

    [Feature] 1. FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+from base_caching import BaseCaching
+"""
+FIFO caching
+"""
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFO cache inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """
+        initialiaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item from cache
+        """
+        self.cache_data[key] = item
+        if key is not None or item is not None:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                keydis = list(self.cache_data.keys())[0]
+                del self.cache_data[keydis]
+                print("DISCARD: {}".format(keydis))
+
+    def get(self, key):
+        """
+        Get items from cache
+        """
+        if key is not None or key:
+            self.cache_data.get(key)
