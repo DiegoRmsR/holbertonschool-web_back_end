4db30c309302b351d444e0e7a6537b3dbd63a035
DiegoRmsR <diegoramos.mt@gmail.com>
2020-10-27 15:28:04 -0500

    [Feature] 0. Simple helper function

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -7,6 +7,13 @@ from typing import Tuple
 
 
 def index_range(page, page_size) -> Tuple[int, int]:
+    """
+    Args:
+        page: index one
+        page_size: index two
+    Return:
+        Tuple of int
+    """
     start_index = (page - 1) * page_size
     end_index = page_size * page
     return (start_index, end_index)
