77b6b8487aba9c45addef8462e89af963fa94be3
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-11-12 04:13:55 +0000

    [Feature] Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -70,15 +70,18 @@ def before_request() -> str:
 
     excluded_path = ['/api/v1/status/',
                      '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+                     '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login/']
 
     if not (auth.require_auth(request.path, excluded_path)):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
+
     if (current_user) is None:
         abort(403)
 
